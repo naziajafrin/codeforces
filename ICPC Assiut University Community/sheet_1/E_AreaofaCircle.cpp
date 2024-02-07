d012977fda019d64da45364d56e479b368fc930d
Nazia <nazia.jafrin1@gmail.com>
2024-02-08 01:21:56 +0600

    area of circle

---
+++ ICPC Assiut University Community/sheet_1/E_AreaofaCircle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  float f;
+  cin >> f;
+  double result = 3.141592653 * f * f;
+  cout << fixed << setprecision(9);
+  cout << result << endl;
+  
+
+  return 0;
+}
