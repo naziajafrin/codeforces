8a74c46bc0b7e2c968a5a7e51a17292a5abd9bfe
Nazia <nazia.jafrin1@gmail.com>
2024-02-08 00:07:53 +0600

    show datatype

---
+++ ICPC Assiut University Community/sheet_1/B_BasicDataTypes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+  int n; cin >> n;
+  long long li; cin >> li;
+  char c ; cin >> c;
+  float f; cin >> f;
+  double d; cin >> d;
+  
+  cout << n << endl;
+  cout << li << endl;
+  cout << c << endl;
+  cout << f << endl;
+  cout << d << endl;
+  return 0;
+}
