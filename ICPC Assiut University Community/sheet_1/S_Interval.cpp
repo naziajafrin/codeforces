3a6a3d5470cdcb5847ba9289f9e51c3e575e3208
Nazia <nazia.jafrin1@gmail.com>
2024-02-09 22:38:12 +0600

    range

--- ICPC Assiut University Community/sheet_1/S_Interval.cpp
+++ ICPC Assiut University Community/sheet_1/S_Interval.cpp
@@ -7,7 +7,7 @@ int main(){
   if(n >= 0 && 25 >= n) cout << "Interval [0,25]" << endl;
   else if(n > 25 && 50 >= n) cout << "Interval (25,50]" << endl;
   else if(n > 50 &&  75 >= n) cout << "Interval (50,75]" << endl;
-  else if(n >75 && 25 >= 100) cout << "Interval (75,100]" << endl;
+  else if(n >75 && 100 >= n) cout << "Interval (75,100]" << endl;
   else cout << "Out of Intervals" << endl;
   return 0;
 }
