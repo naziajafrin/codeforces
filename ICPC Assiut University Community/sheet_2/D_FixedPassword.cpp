ae0190c037a29dc701d262de4f2757e8c92e831d
Nazia <nazia.jafrin1@gmail.com>
2024-02-15 08:17:03 +0600

    wrong correct cout

---
+++ ICPC Assiut University Community/sheet_2/D_FixedPassword.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  while(1){
+    int n; cin >> n;
+
+    if(1999 == n) {
+      cout << "Correct" << endl;
+      break;
+    }
+    else cout << "Wrong" << endl;
+  }
+  return 0;
+}
